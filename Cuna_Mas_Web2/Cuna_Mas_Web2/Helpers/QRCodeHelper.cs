a53e7ddfb011584680098b4bf151b51004a63fea
Enzo Catalán Vargas <enzocv96@gmail.com>
2018-11-18 18:38:21 -0500

    qr code generado, pero un no guarda en BD

---
+++ Cuna_Mas_Web2/Cuna_Mas_Web2/Helpers/QRCodeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+
+namespace Cuna_Mas_Web2.Helpers
+{
+    public class QRCodeHelper
+    {
+        public Image GenerateQRCode(string content, int size)
+        {
+            var encoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode;
+            encoder.TryEncode(content, out qrCode);
+
+            var gRenderer = new GraphicsRenderer(new FixedModuleSize(4, QuietZoneModules.Two), Brushes.Black, Brushes.White);
+            //Graphics g = gRenderer.Draw(qrCode.Matrix);
+
+            var ms = new MemoryStream();
+            gRenderer.WriteToStream(qrCode.Matrix, ImageFormat.Bmp, ms);
+
+            var imageTemp = new Bitmap(ms);
+
+            var image = new Bitmap(imageTemp, new Size(new Point(size, size)));
+
+            //image.Save("file.bmp", ImageFormat.Bmp);
+
+            return image;
+        }
+    }
+}
